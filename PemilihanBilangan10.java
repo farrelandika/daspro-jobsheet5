cd8ffaf034d8950029131223ee6b31047a5a712e
unknown <farrelandika14@gmail.com>
2024-09-25 07:23:07 +0800

    bilangan genap dan ganjil jobsheet

---
+++ PemilihanBilangan10.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PemilihanBilangan10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan Sebuah Angka: ");
+        int angka = sc.nextInt();
+        if (angka % 2 == 0)
+        {
+            System.out.println("Angka " + angka + " termasuk bilangan genap");
+        }
+        else
+        {
+            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
+        }
+        
+
+    }
+}
